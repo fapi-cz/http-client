2627514911367c8fd5d9597a5b5c03e3a309e353
Jiří Slischka <slischkaj@gmail.com>
2018-11-16 19:18:57 +0100

    Added TracyToPsrLogger

---
+++ src/Fapi/HttpClient/Bridges/Tracy/TracyToPsrLogger.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types = 1);
+
+namespace Fapi\HttpClient\Bridges\Tracy;
+
+use Psr\Log\AbstractLogger;
+use Psr\Log\LogLevel;
+use Tracy\ILogger;
+
+final class TracyToPsrLogger extends AbstractLogger
+{
+
+	/** PSR-3 log level to Tracy logger priority mapping */
+	const PRIORITY_MAP = [
+		LogLevel::EMERGENCY => ILogger::CRITICAL,
+		LogLevel::ALERT => ILogger::CRITICAL,
+		LogLevel::CRITICAL => ILogger::CRITICAL,
+		LogLevel::ERROR => ILogger::ERROR,
+		LogLevel::WARNING => ILogger::WARNING,
+		LogLevel::NOTICE => ILogger::WARNING,
+		LogLevel::INFO => ILogger::INFO,
+		LogLevel::DEBUG => ILogger::DEBUG,
+	];
+
+	/** @var ILogger */
+	private $tracyLogger;
+
+	public function __construct(ILogger $tracyLogger)
+	{
+		$this->tracyLogger = $tracyLogger;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function log($level, $message, array $context = [])
+	{
+		$priority = self::PRIORITY_MAP[$level] ?? ILogger::ERROR;
+
+		if (isset($context['exception']) && $context['exception'] instanceof \Throwable) {
+			$this->tracyLogger->log($context['exception'], $priority);
+			unset($context['exception']);
+		}
+
+		if ($context) {
+			$message = [
+				'message' => $message,
+				'context' => $context,
+			];
+		}
+
+		$this->tracyLogger->log($message, $priority);
+	}
+
+}

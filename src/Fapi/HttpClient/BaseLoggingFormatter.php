68b62e44e5809798608b5b9a1174846f8244eedf
Jiří Slischka <slischkaj@gmail.com>
2022-04-01 12:06:44 +0200

    Added max body log length to BaseFormatter

--- src/Fapi/HttpClient/BaseLoggingFormatter.php
+++ src/Fapi/HttpClient/BaseLoggingFormatter.php
@@ -8,13 +8,25 @@ use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Throwable;
 use function base64_encode;
+use function extension_loaded;
+use function function_exists;
 use function get_class;
+use function iconv_substr;
+use function mb_substr;
 use function serialize;
 use function sprintf;
 use const JSON_UNESCAPED_UNICODE;
 
 final class BaseLoggingFormatter implements ILoggingFormatter
 {
+
+	/** @var int */
+	private $maxBodyLength;
+
+	public function __construct(int $maxBodyLength = 40000)
+	{
+		$this->maxBodyLength = $maxBodyLength;
+	}
 
 	public function formatSuccessful(RequestInterface $request, ResponseInterface $response, float $elapsedTime): string
 	{
@@ -34,17 +46,21 @@ final class BaseLoggingFormatter implements ILoggingFormatter
 
 	private function dumpHttpRequest(RequestInterface $request): string
 	{
+		$body = $this->processBody((string) $request->getBody());
+
 		return ' Request URL: ' . $this->dumpValue((string) $request->getUri())
 			. ' Request method: ' . $this->dumpValue($request->getMethod())
 			. ' Request headers: ' . $this->dumpValue($request->getHeaders())
-			. ' Request body: ' . $this->dumpValue((string) $request->getBody());
+			. ' Request body: ' . $this->dumpValue($body);
 	}
 
 	private function dumpHttpResponse(ResponseInterface $response): string
 	{
+		$body = $this->processBody((string) $response->getBody());
+
 		return ' Response status code: ' . $this->dumpValue($response->getStatusCode())
 			. ' Response headers: ' . $this->dumpValue($response->getHeaders())
-			. ' Response body: ' . $this->dumpValue((string) $response->getBody());
+			. ' Response body: ' . $this->dumpValue($body);
 	}
 
 	private function dumpException(Throwable $exception): string
@@ -79,6 +95,23 @@ final class BaseLoggingFormatter implements ILoggingFormatter
 		} catch (JsonException $e) {
 			return '(serialized) ' . base64_encode(serialize($value));
 		}
+	}
+
+	private function processBody(string $body): string
+	{
+		if (strlen($body) <= $this->maxBodyLength) {
+			return $body;
+		}
+
+		if (function_exists('mb_substr')) {
+			return mb_substr($body, 0, $this->maxBodyLength, 'UTF-8');
+		}
+
+		if (!extension_loaded('iconv')) {
+			return iconv_substr($body, 0, $this->maxBodyLength, 'UTF-8');
+		}
+
+		return substr($body, 0, $this->maxBodyLength);
 	}
 
 }
